4df3322db1a73643ee95fffb96d40b6e9f57c775
Kamil Andziak <kamil.andziakk97@gmail.com>
2021-11-20 16:17:00 +0100

    zadanka

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-
+const button2 = document.getElementById("buton2");
 let naszaliczba = prompt("wybierz liczbe")
 const button = document.getElementById("buton1").addEventListener("click", wyswietlanie);
+
 // button.preventDefault()
 function wyswietlanie(){
     if(naszaliczba <= 12){
@@ -8,3 +9,66 @@ function wyswietlanie(){
     else {
         console.log("To zbior mniejszy od 12")}
 }
+
+class Card {
+    constructor(value, type) {
+        this.value = value;
+        this.type= type;
+        this.drawCard();
+    }
+    drawCard(){
+        const card = document.createElement("div");
+        card.setAttribute("class", `card`);
+        card.setAttribute("value", `${this.value} ${this.type}`);
+        card.innerHTML = `${this.value} ${this.type}`;
+        return card;
+    }
+}
+const value = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "D",
+    "K",
+]
+
+const type = ["&clubs;" , "&diams;", 	"&hearts;" , "&spades;" ]
+
+
+ class Deck{
+
+
+    cards = [];
+    constructor() {
+        this.createDeck();
+        this.schaffle();
+        this.pickOne();
+    }
+    createDeck(){
+        value.forEach(v => type.forEach( t => this.cards.push(new Card(v, t))));
+    }
+    schaffle(){
+        for(let i = this.cards.length  -1 ; i >0 ; i--){
+            const j = Math.floor(Math.random()*i)
+            const wyjebana = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = wyjebana;
+        }
+
+    }
+    pickOne(){
+        console.log( this.cards.pop());
+    }
+}
+const deckk = new Deck();
+// deckk.pickOne()
+
+button2.addEventListener("click", () => deckk.pickOne());
