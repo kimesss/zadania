759a7b641ed59e26805fff440ee3ba3ac8606ee4
Kamil Andziak <kamil.andziakk97@gmail.com>
2021-11-20 15:26:06 +0100

    zadanka

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let naszaliczba = prompt("wybierz liczbe")
+const button = document.getElementById("buton1").addEventListener("click", wyswietlanie);
+// button.preventDefault()
+function wyswietlanie(){
+    if(naszaliczba <= 12){
+        console.log("To zbior mniejszy od 12")}
+    else {
+        console.log("To zbior mniejszy od 12")}
+}
